a77645a3d7473b3c7b8ff7f6855c5a216eb1e57a
Arunkumar1992 <arunkumareee7@gmail.com>
2016-11-24 02:16:54 +0530

    Update slackbothome.php

--- pages/slackbothome.php
+++ pages/slackbothome.php
@@ -3,8 +3,10 @@
 
 $datain = file_get_contents('php://input');
 $obj = json_decode($datain);
-  print_r($obj);
-
+//  print_r($obj);
+if(array_key_exists('challenge', $obj)) {
+  print_r($obj->challenge);
+}
 
 
 
