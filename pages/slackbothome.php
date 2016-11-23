7001f10ac7b94900f56de1480b84740d62c61278
Arunkumar1992 <arunkumareee7@gmail.com>
2016-11-24 01:45:38 +0530

    Update slackbothome.php

--- pages/slackbothome.php
+++ pages/slackbothome.php
@@ -1,17 +1,14 @@
 
 <?php
 echo "In a slack bot handler page";
-echo $_POST;
-echo "new post printed";
+
 $datain = file_get_contents('php://input');
 $obj = json_decode($datain);
-print_r(json_encode($datain));
-echo "new datain printed";
-echo $_GET;
-echo "new get printed";
+print_r($obj);
 
-echo $obj;
-echo "new obj printed";
+
+
+
 
 ?>
 
