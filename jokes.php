b00e7ec23833696862d4fd22cd73ac85b1d6e375
Arunkumar1992 <arunkumareee7@gmail.com>
2016-12-07 16:51:14 +0530

    Create jokes.php

---
+++ jokes.php
@@ -0,0 +1,13 @@
+<?php
+$url = 'http://api.icndb.com/jokes/random/';
+
+$ch = curl_init( $url );
+curl_setopt( $ch, CURLOPT_POST, 1);
+curl_setopt( $ch, CURLOPT_POSTFIELDS, "");
+curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt( $ch, CURLOPT_HEADER, 0);
+curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
+
+$response = curl_exec( $ch );
+echo $response;
+?>
