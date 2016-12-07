1922f2e34058abc02dda544f876fd6efce1b0529
Arunkumar1992 <arunkumareee7@gmail.com>
2016-12-07 17:07:52 +0530

    Update jokes.php

--- jokes.php
+++ jokes.php
@@ -1,26 +1,17 @@
 <?php
 $url = 'http://api.icndb.com/jokes/random/';
-echo "joke will print 0";
-
 $ch = curl_init( $url );
 curl_setopt( $ch, CURLOPT_POST, 1);
 curl_setopt( $ch, CURLOPT_POSTFIELDS, "");
 curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
 curl_setopt( $ch, CURLOPT_HEADER, 0);
 curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
-
 $response = curl_exec( $ch );
-echo $response;
-echo "joke will print 1";
+$data = json_decode($response, TRUE);
 
-$obj = json_decode($response);
-echo "joke will print 2";
+$joke = $data['value']['joke'];
+echo '{ "joke": "' .$joke . '"}';
 
-$joke = $obj['value']['joke'];
-echo "joke will print 3";
 
-echo $joke;
-echo "joke printed";
 //  print_r($obj);
-
 ?>
